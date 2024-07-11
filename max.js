c674a59a0f29be042bd72dfe5f2e0603994e6c81
Ridham666 <148061124+Ridham666@users.noreply.github.com>
2024-07-11 17:07:55 +0530

    Add files via upload

---
+++ max.js
@@ -0,0 +1,19 @@
+var a=17
+var b=23
+var c=12
+if(a>b)
+{   
+    if(a>c)
+    {
+        document.write(a)
+    }else{
+        document.write(c)
+    }
+}else{
+    if(b>c)
+    {
+        document.write(b)
+    }else{
+        document.write(c)
+    }
+}
